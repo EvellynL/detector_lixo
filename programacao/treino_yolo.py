f5ea2e22db9df46d0c75c405e714439272517edd
Evellyn PC <evellserrao@icloud.com>
2026-01-21 22:48:21 -0300

    feat: adição do terceiro modelo e diagrama eletrico no fritizing

--- programacao/treino_yolo.py
+++ programacao/treino_yolo.py
@@ -4,13 +4,17 @@ def main():
     model = YOLO("yolov8n.pt")
     model.train(
         data="programacao\dataset_yolov8\data.yaml",
-        epochs=250,
-        imgsz=416,
+        epochs=1000,
+        imgsz=320,
         batch=16,
-        workers=0,
+        workers=4,
         lr0= 0.005,
         cos_lr= True,
-        iou= 0.6
+        iou= 0.6,
+        optimizer = 'AdamW',
+        weight_decay=0.0005,
+        conf = 0.5
+
     )
 
 if __name__ == "__main__":
