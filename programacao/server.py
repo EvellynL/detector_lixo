ea9873116a5a9f1e63ae3134bc8931733a9926e8
Rasp da eve <els.eai23@uea.edu.br>
2026-05-29 19:31:18 -0400

    feat: adicao de novos status

--- programacao/server.py
+++ programacao/server.py
@@ -8,6 +8,16 @@ SERVER = '192.169.38.77'
 
 CONNECT_URL = f'http://{SERVER}:3000/api/connect'
 CLASSIFICATION = f'http://{SERVER}:3000/api/classification'
+STATUS_URL = f'http://{SERVER}:3000/api/systemStatus'
+
+def send_status(status):
+    requests.post(
+        STATUS_URL,
+
+        json={
+            'status': status
+        }
+    )
 
 model = YOLO('runs/detect/train/weights/best_100epochs.pt')
 path = 'programacao/dataset_yolov8/test/images'
@@ -15,6 +25,8 @@ path = 'programacao/dataset_yolov8/test/images'
 while True:
     try:
         requests.post(CONNECT_URL)
+
+        send_status('Iniciando detecçao')
 
         nome_img = random.choice(os.listdir(path))
         caminho = os.path.join(path, nome_img)
@@ -53,8 +65,32 @@ while True:
                     print(
                         'Erro ao enviar a imagem'
                     )
+            
+            send_status(f'{class_name} detectado')
+
+            time.sleep(5)
+
+            send_status('Realizando a triagem')
+            
+            time.sleep(5)
+            
+            send_status('Triagem finzalizada')
+
+            time.sleep(5)
+
+            send_status('Aguardando nova detecçao')
+
+            time.sleep(10)
         else:
             print('Nenhuma classe detectada')
+
+            send_status('Nenhuma classe encontrada')
+
+            time.sleep(5)
+
+            send_status('Aguardando nova detecçao')
+
+            time.sleep(10)
 
          
     except Exception as error:
