2916b651291d17a411446d4b935125ee2c5a626f
Evellyn PC <evellserrao@icloud.com>
2026-07-02 21:07:46 -0300

    treinos

--- programacao/dataset_yolov8/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# trash detector > 2026-01-21 10:59am
-https://universe.roboflow.com/garbarge-detection/trash-detector-xvu2c
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- programacao/treino_yolo.py
+++ programacao/treino_yolo.py
@@ -3,19 +3,25 @@ from ultralytics import YOLO
 def main():
     model = YOLO("yolov8n.pt")
     model.train(
-        data="programacao\dataset_yolov8\data.yaml",
-        epochs=1000,
-        imgsz=320,
-        batch=16,
-        workers=4,
-        lr0= 0.005,
+        data="programacao\dataset_yolov8_640\data.yaml",
+        epochs=300,
+        imgsz=640,
+        batch=10,
+        workers=8,
+        lr0= 0.01,
         cos_lr= True,
-        iou= 0.6,
+        iou= 0.5,
         optimizer = 'AdamW',
+        lrf = 0.1,
         weight_decay=0.0005,
-        conf = 0.5
+        hsv_h=0.015,
+        hsv_s=0.7,
+        hsv_v=0.4,
+        mosaic=1.0,
+        mixup=0.1,
 
     )
+
 
 if __name__ == "__main__":
     main()
